6a828b752af32c9cac0233185147b22f84aa081d
petrikovandrey <aapetrikov@gmail.com>
2021-07-17 14:37:13 +0300

    task 2

--- src/basic.test.js
+++ src/basic.test.js
@@ -1,38 +1,9 @@
 import * as index from './index';
 
-test('attack stoned', () => {
-    const gena = new index.Magician('Gena');
-    gena.stoned = true;
-    gena.distance = 2;
-    gena.attack = 100;
-    expect(gena.attack).toBe(85);
-
-});
-
-test('attack not stoned 4', () => {
-    const gena = new index.Magician('Gena');
-    gena.stoned = false;
-    gena.distance = 4;
-    gena.attack = 100;
-    expect(gena.attack).toBe(60);
-
-});
-
-test('attack not stoned 1', () => {
-    const gena = new index.Magician('Gena');
-    gena.stoned = false;
-    gena.distance = 1;
-    gena.attack = 100;
-    expect(gena.attack).toBe(100);
-
-});
-
-
-test('attack not stoned 0', () => {
-    const gena = new index.Magician('Gena');
-    gena.stoned = false;
-    gena.distance = 0;
-    gena.attack = 100;
-    expect(gena.attack).toBe(100);
+test('buffer', () => {
+    const aBuffer = new index.ArrayBufferConverter();
+    aBuffer.load(index.getBuffer());
+    const result = aBuffer.toString();
+    expect(result).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
 
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 class ArrayBufferConverter {
     constructor() {
-
+  
     }
-    load() {
-
+    load(buffer) {
+      this.buffer = buffer;
     }
     toString() {
-
+     return String.fromCharCode.apply(null, new Uint16Array(this.buffer));
     }
-}
+  }
+  
+  function getBuffer() {
+    const data = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+    return (input => {
+      const buffer = new ArrayBuffer(data.length * 2);
+      const bufferView = new Uint16Array(buffer);
+      for (let i = 0; i < input.length; i++) {
+        bufferView[i] = input.charCodeAt(i);
+      }
+      return buffer;
+    })(data);
+  }
+  
+export { getBuffer, ArrayBufferConverter };
